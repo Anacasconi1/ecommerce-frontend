df382848af16778f3ed1dbf3913ae2bba8939ebe
Anacasconi1 <sabriicasconii@gmail.com>
2024-09-20 18:23:14 -0300

    Proyect inicialization

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+    return (
+        <>
+            <div className="flex justify-between columns-4 bg-esmerald-100 text-black">
+                <div>
+                    <h1>Hola tailwind</h1>
+                </div>
+                <div>
+                    <h1>Otro texto</h1>
+                </div>
+            </div>
+        </>
+    );
+}
